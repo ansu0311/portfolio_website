ea2542a4e4831cb6fa6ab96a9ea8935deb7adc46
Ansuman Nayak <nsatyam95@gmail.com>
2023-11-04 11:14:46 +0530

    Responsive setting updates

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,43 +1,44 @@
-import React from 'react'
-import "./Hero.css"
-import {motion} from 'framer-motion'
+import React from "react";
+import "./Hero.css";
 const Hero = () => {
-
-  let newDate = new Date()
+  let newDate = new Date();
   let date = newDate.getDate();
   let month = newDate.getMonth();
 
-  const monthName = (num) =>{
+  const monthName = (num) => {
     const date = new Date();
     date.setMonth(num);
-    var month = date.toLocaleString('default', { month: 'short' });
-    return month
-  }
+    var month = date.toLocaleString("default", { month: "short" });
+    return month;
+  };
 
   return (
-    <div className='Hero' id='home'>
-      <div className='HeroContainer'>
+    <div className="Hero" id="home">
+      <div className="HeroContainer">
         <div className="HeroLeft">
-          <div className='Hero_left_text1'>creative</div>
-          <div className='Hero_left_text2'>
+          <div className="Hero_left_text1">creative</div>
+          <div className="Hero_left_text2">
             designer<span> &</span> Developer
           </div>
         </div>
-        <div className='HeroRight'>
+        <div className="HeroRight">
           <div className="available">
-            <div className='date'>{date}</div>
-            <div className='date_text'><span>{monthName(month)}</span>available for work</div>
-          </div>
-          <div>
-          <div className='hero_discription'>
-            I'm a front-end developer and UI/UX designer. I like minimalist design that grabs attention. I love nature, anime, food and art
+            <div className="date">{date}</div>
+            <div className="date_text">
+              <span>{monthName(month)}</span>available for work
             </div>
-          <div className='hero_button'>Contact me</div>
+          </div>
+          <div className="hero_layout">
+            <div className="hero_discription">
+              I'm a front-end developer and UI/UX designer. I like minimalist
+              design that grabs attention. I love nature, anime, food and art
+            </div>
+            <div className="hero_button">Contact me</div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
